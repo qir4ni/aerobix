88670e5e72cbb3bd385e9f930ffab5cb05304714
qir4ni <qiraniawis@gmail.com>
2024-12-21 01:50:21 +0800

    Add time in workout_session table

---
+++ database/migrations/2024_12_20_174555_add_time_columns_in_workout_sessions_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('workout_sessions', function (Blueprint $table) {
+            $table->timestamp('start_time')->nullable();
+            $table->timestamp('end_time')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('workout_sessions', function (Blueprint $table) {
+            $table->dropColumn('start_time');
+            $table->dropColumn('end_time');
+        });
+    }
+};
