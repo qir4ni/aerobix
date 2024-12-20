4e7f242b0c67cbeff2b5619b46a4bc383f5d24bd
qir4ni <qiraniawis@gmail.com>
2024-12-21 00:33:58 +0800

    Create WorkoutSession Resource

---
+++ app/Filament/Resources/WorkoutSessionResource.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\WorkoutSessionResource\Pages;
+use App\Filament\Resources\WorkoutSessionResource\RelationManagers;
+use App\Models\WorkoutSession;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class WorkoutSessionResource extends Resource
+{
+    protected static ?string $model = WorkoutSession::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListWorkoutSessions::route('/'),
+            'create' => Pages\CreateWorkoutSession::route('/create'),
+            'edit' => Pages\EditWorkoutSession::route('/{record}/edit'),
+        ];
+    }
+}
---
+++ app/Filament/Resources/WorkoutSessionResource/Pages/CreateWorkoutSession.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Resources\WorkoutSessionResource\Pages;
+
+use App\Filament\Resources\WorkoutSessionResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateWorkoutSession extends CreateRecord
+{
+    protected static string $resource = WorkoutSessionResource::class;
+}
