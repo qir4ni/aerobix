b805b076b2b5811912a860f41e1c7f1735b3c0f5
qir4ni <qiraniawis@gmail.com>
2024-12-22 17:43:17 +0800

    Adding View Attendance Page

--- app/Filament/Resources/AttendanceResource.php
+++ app/Filament/Resources/AttendanceResource.php
@@ -5,11 +5,17 @@ namespace App\Filament\Resources;
 use App\Filament\Resources\AttendanceResource\Pages;
 use App\Filament\Resources\AttendanceResource\RelationManagers;
 use App\Models\Attendance;
+use App\Models\User;
+use App\Models\WorkoutSession;
 use Filament\Forms;
+use Filament\Forms\Components\DateTimePicker;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
+use Filament\Forms\Components\Select;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Columns\IconColumn;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
 
@@ -17,13 +23,21 @@ class AttendanceResource extends Resource
 {
     protected static ?string $model = Attendance::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-clipboard-document-list';
 
     public static function form(Form $form): Form
     {
         return $form
             ->schema([
-                //
+                Select::make('user_id')
+                    ->label('User')
+                    ->options(User::all()->pluck('name', 'id'))
+                    ->searchable(),
+                Select::make('workout_session_id')
+                    ->label('Workout Session')
+                    ->options(WorkoutSession::all()->pluck('session_name', 'id'))
+                    ->searchable(),
+                DateTimePicker::make('attended_at'),
             ]);
     }
 
@@ -31,7 +45,12 @@ class AttendanceResource extends Resource
     {
         return $table
             ->columns([
-                //
+                TextColumn::make('user.name'),
+                TextColumn::make('workout_session.session_name'),
+                TextColumn::make('attended_at')
+                    ->dateTime('M d, Y g:i A'),
+                // IconColumn::make('is_featured')
+                // ->boolean(),
             ])
             ->filters([
                 //
--- app/Filament/Resources/AttendanceResource/Pages/ListAttendances.php
+++ app/Filament/Resources/AttendanceResource/Pages/ListAttendances.php
@@ -3,6 +3,7 @@
 namespace App\Filament\Resources\AttendanceResource\Pages;
 
 use App\Filament\Resources\AttendanceResource;
+use App\Models\Attendance;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
 
