eeb6d283d4ec613c2b9babde3158d6c38be58775
qir4ni <qiraniawis@gmail.com>
2024-12-22 17:44:29 +0800

    Update Workout Session Resource

--- app/Filament/Resources/WorkoutSessionResource.php
+++ app/Filament/Resources/WorkoutSessionResource.php
@@ -6,24 +6,37 @@ use App\Filament\Resources\WorkoutSessionResource\Pages;
 use App\Filament\Resources\WorkoutSessionResource\RelationManagers;
 use App\Models\WorkoutSession;
 use Filament\Forms;
+use Filament\Forms\Components\DatePicker;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\TimePicker;
+use Filament\Tables\Columns\TextColumn;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Filament\Tables\Filters\Filter;
+use Malzariey\FilamentDaterangepickerFilter\Filters\DateRangeFilter;
 
 class WorkoutSessionResource extends Resource
 {
     protected static ?string $model = WorkoutSession::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-clock';
 
     public static function form(Form $form): Form
     {
         return $form
             ->schema([
-                //
+                TextInput::make('session_name')
+                    ->label('Session Name'),
+                DatePicker::make('session_date')
+                    ->label('Session Date'),
+                TimePicker::make('start_time')
+                    ->label('Start Time'),
+                TimePicker::make('end_time')
+                    ->label('End Time'),
             ]);
     }
 
@@ -31,10 +44,17 @@ class WorkoutSessionResource extends Resource
     {
         return $table
             ->columns([
-                //
+                TextColumn::make('id'),
+                TextColumn::make('session_name'),
+                TextColumn::make('session_date')
+                    ->date(),
+                TextColumn::make('start_time')
+                    ->time('g:i A'),
+                TextColumn::make('end_time')
+                    ->time('g:i A'),
             ])
-            ->filters([
-                //
+            ->filters([ 
+                DateRangeFilter::make('session_date'),
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
